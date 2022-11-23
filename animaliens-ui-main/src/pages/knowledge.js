22a62dde2b50f8140946475166f4cd92008bf0e8
A. Developers <ahmarjabbar7@gmail.com>
2022-11-23 13:35:29 +0500

    pages update

--- animaliens-ui-main/src/pages/knowledge.js
+++ animaliens-ui-main/src/pages/knowledge.js
@@ -1,11 +1,14 @@
 import React from "react";
+import Content from "../components/Coursecontent/content";
+import KnowledgeTrending from "../components/KnowledgeCards/trending";
+import Video from "../components/Video/video";
 
 const Knowledge = () => {
   return (
     <div className="css-n1ozge">
-      <Carousel />
-      <Trending />
-      <Proyects />
+      <Video />
+      <Content />
+      <KnowledgeTrending />
     </div>
   );
 };
